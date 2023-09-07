8ae6631771809da08ec81ae4d3140e74f29d3542
Bertin <irankundab21ertin@gmail.com>
2023-09-07 07:56:02 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+/**
+*main - main function
+*Return: always 1
+*/
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main (void)
+{
+	int stlength;
+	char *words = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	stlength = strlen(words);
+	write(1, words, stlength);
+	return (1);
+}
