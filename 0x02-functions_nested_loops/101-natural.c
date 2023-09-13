010f70ed5bf798a6b59cab3842eb2fcf091ac274
Bertin <irankundab21ertin@gmail.com>
2023-09-13 13:48:12 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - main function
+*Return: always 0
+*/
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 1 ; i < 10 ; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+
+	}
+
+	printf("%d\n", sum);
+}
