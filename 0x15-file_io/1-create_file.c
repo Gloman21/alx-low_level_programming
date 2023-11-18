8b0597cf6645b4f83cbe7ae6f390b863048f1319
Bertin <irankundab21ertin@gmail.com>
2023-11-17 23:50:24 -0800

    fixed 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,37 @@
 #include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+
 /**
 *create_file - creates and writes into files
 *@filename: file name to create
-@text_content: the content to be copied into the file
+*@text_content: the content to be copied into the file
 *Return: 1 if successful or -1 if it fails
 */
 int create_file(const char *filename, char *text_content)
 {
 	int file_descriptor;
-	int number_letters;
-	int write_return;
+	ssize_t written_bytes;
+	ssize_t number_letters = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file_descriptor == -1)
 		return (-1);
 
 	if (text_content == NULL)
-		text_content = " ";
+		text_content = "";
 	for (number_letters = 0; text_content[number_letters]; number_letters++)
+	{
 
-	write_return = write(file_descriptor, text_content, number_letters);
+	}
 
-	if (write_return == -1)
+	written_bytes = write(file_descriptor, text_content, number_letters);
+
+	if (written_bytes == -1)
 		return (-1);
 	close(file_descriptor);
 
